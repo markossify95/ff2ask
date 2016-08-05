b59ea9ec3056b1306cbb60f6a23adbc2a1fd9d9f
markossify95 <markoss95@hotmail.com>
2016-08-05 12:25:32 +0200

    Finished most of neccessary views for teacher part of interaction, created url patterns

--- teachers/urls.py
+++ teachers/urls.py
@@ -6,5 +6,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns = [
     url(r'^all/$', view=views.TeacherList.as_view(), name='all-teachers'),
     url(r'^(?P<pk>[0-9]+)/$', views.TeacherInstance.as_view(), name='teacher-instance'),
-    url(r'^(?P<fk>[0-9]+)/questions/$', views.QuestionList.as_view(), name='questions-by-teacher'),
+    url(r'^(?P<fk>[0-9]+)/questions/$', views.QuestionList.as_view(), name='t-questions'),
+    url(r'^questions/(?P<pk>[0-9]+)/$', views.QuestionInstance.as_view(), name='t-question-instance'),
+    url(r'^questions/(?P<fk>[0-9]+)/answers/$', views.AnswerList.as_view(), name='t-question-answers'),
+    url(r'^questions/(?P<fk>[0-9]+)/answers/(?P<pk>[0-9]+)/$', views.AnswerInstance.as_view(), name='t-answer-instance'),
 ]
--- teachers/views.py
+++ teachers/views.py
@@ -61,10 +61,89 @@ class QuestionList(APIView):
             'teacher_id': fk,
             'tq_type': request.data['tq_type'],
             'tq_text': request.data['tq_text']  # OVAKO DOKTOR IZVLACI PODATKE
-        }
+        }  # PAZI STA SALJES IZ FRONTA OVAMO, MORA DA BUDE DOBRO STRUKTUIRANO!
         serializer = TeacherQuestionSerializer(data=d)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class QuestionInstance(APIView):
+    def get_object(self, pk):
+        try:
+            return TeacherQuestion.objects.get(pk=pk)
+        except TeacherQuestion.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        question = self.get_object(pk)
+        serializer = TeacherQuestionSerializer(question)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        question = self.get_object(pk)
+        serializer = TeacherQuestionSerializer(question, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        question = self.get_object(pk)
+        question.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# GETs all answers for particular question, POSTs new answer
+class AnswerList(APIView):
+    def get(self, request, fk):
+        answers = TQAnswer.objects.filter(question_id=fk)
+        serializer = TQAnswerSerializer(answers, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, fk):
+        d = {
+            'question_id': fk,
+            'tq_answer_text': request.data['tq_answer_text'],
+            'score': request.data['score']
+        }  # PAZI STA SALJES IZ FRONTA OVAMO, MORA DA BUDE DOBRO STRUKTUIRANO!
+        serializer = TQAnswerSerializer(data=d)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class AnswerInstance(APIView):
+    def get_object(self, pk):
+        try:
+            return TQAnswer.objects.get(pk=pk)
+        except TQAnswer.DoesNotExist:
+            raise Http404
+
+    def get(self, request, fk, pk):
+        answer = self.get_object(pk)
+        serializer = TQAnswerSerializer(answer)
+        return Response(serializer.data)
+
+    def put(self, request, pk, fk):
+        answer = self.get_object(pk)
+        d = {
+            'id': pk,
+            'question_id': fk,
+            'tq_answer_text': request.data['tq_answer_text'],
+            'score': request.data['score']
+        }
+        serializer = TQAnswerSerializer(answer, data=d)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, fk):
+        answer = self.get_object(pk)
+        answer.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
